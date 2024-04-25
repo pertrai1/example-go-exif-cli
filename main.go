41795e6021e2085f7fced27691a4affca2b4c507
Rob Simpson <rsimpson2@me.com>
2024-04-24 21:23:29 -0400

    update code for geocode

--- main.go
+++ main.go
@@ -1,99 +1,155 @@
 package main
 
 import (
-    "bytes"
-    "encoding/csv"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "path/filepath"
-    "strconv"
-    "strings"
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+    "net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
-    "github.com/barasher/go-exiftool"
+	"github.com/barasher/go-exiftool"
 )
 
+// Convert DMS (Degrees, Minutes, Seconds) coordinates to decimal degrees
+func convertDMSToDecimal(dms string, direction string) (float64, error) {
+	parts := strings.FieldsFunc(dms, func(r rune) bool {
+		return r == ' ' || r == 'd' || r == 'e' || r == 'g' || r == '\'' || r == '"'
+	})
+
+	degrees, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		return 0, err
+	}
+
+	minutes, err := strconv.ParseFloat(parts[1], 64)
+	if err != nil {
+		return 0, err
+	}
+
+	seconds, err := strconv.ParseFloat(parts[2], 64)
+	if err != nil {
+		return 0, err
+	}
+
+	decimal := degrees + minutes/60 + seconds/3600
+
+	if direction == "S" || direction == "W" {
+		decimal = -decimal
+	}
+	return decimal, nil
+}
+
+// Perform reverse geocoding using the provided longitude and latitude
 func reverseGeocode(longitude, latitude float64) (string, error) {
-    baseURL := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode"
-    params := fmt.Sprintf("?location=%f,%f&outSR=4326&f=json", longitude, latitude)
-    response, err := http.Get(baseURL + params)
-    if err != nil {
-        return "", err
-    }
-    defer response.Body.Close()
+	// Format the location JSON
+	locationJSON := fmt.Sprintf(`{"x": %f, "y": %f}`, longitude, latitude)
 
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        return "", err
-    }
+	// Define the ArcGIS reverse geocode API URL
+	baseURL := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode"
 
-    var prettyJSON bytes.Buffer
-    if err := json.Indent(&prettyJSON, body, "", "\t"); err != nil {
-        return "", err
-    }
+	// Encode the location JSON as URL query parameter
+	params := fmt.Sprintf("?location=%s&f=json&langCode=en", url.QueryEscape(locationJSON))
 
-    return prettyJSON.String(), nil
+	// Send HTTP GET request to the ArcGIS API
+	response, err := http.Get(baseURL + params)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Format the response body for readability
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, body, "", "\t"); err != nil {
+		return "", err
+	}
+
+	return prettyJSON.String(), nil
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: go run main.go <path_to_directory>")
-        os.Exit(1)
-    }
-    dirPath := os.Args[1]
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <path_to_directory>")
+		os.Exit(1)
+	}
+	dirPath := os.Args[1]
 
-    files, err := ioutil.ReadDir(dirPath)
-    if err != nil {
-        log.Fatal(err)
-    }
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    et, err := exiftool.NewExiftool()
-    if err != nil {
-        log.Fatalf("Error when initializing: %v", err)
-    }
-    defer et.Close()
+	et, err := exiftool.NewExiftool()
+	if err != nil {
+		log.Fatalf("Error when initializing: %v", err)
+	}
+	defer et.Close()
 
-    csvFile, err := os.Create("output.csv")
-    if err != nil {
-        log.Fatalf("Failed creating file: %s", err)
-    }
-    defer csvFile.Close()
+	csvFile, err := os.Create("output.csv")
+	if err != nil {
+		log.Fatalf("Failed creating file: %s", err)
+	}
+	defer csvFile.Close()
 
-    writer := csv.NewWriter(csvFile)
-    defer writer.Flush()
-    writer.Write([]string{"Filename", "Reverse Geocode Response"})
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+	writer.Write([]string{"Filename", "Raw Latitude", "Raw Longitude", "Formatted Latitude", "Formatted Longitude", "Reverse Geocode Response"})
 
-    for _, file := range files {
-        filePath := filepath.Join(dirPath, file.Name())
-        fileExt := strings.ToLower(filepath.Ext(file.Name()))
-        if !file.IsDir() && (fileExt == ".jpg" || fileExt == ".jpeg" || fileExt == ".tif" || fileExt == ".tiff") {
-            fileMetadatas := et.ExtractMetadata(filePath)
-            for _, fileInfo := range fileMetadatas {
-                if fileInfo.Err != nil {
-                    log.Printf("Error concerning %s: %v", fileInfo.File, fileInfo.Err)
-                    continue
-                }
+	for _, file := range files {
+		filePath := filepath.Join(dirPath, file.Name())
+		if !file.IsDir() && isSupportedImageFile(file.Name()) {
+			fileMetadatas := et.ExtractMetadata(filePath)
+			for _, fileInfo := range fileMetadatas {
+				if fileInfo.Err != nil {
+					log.Printf("Error concerning %s: %v", fileInfo.File, fileInfo.Err)
+					continue
+				}
 
-                latitude, _ := strconv.ParseFloat(safeString(fileInfo.Fields, "GPSLatitude"), 64)
-                longitude, _ := strconv.ParseFloat(safeString(fileInfo.Fields, "GPSLongitude"), 64)
-                apiResponse, err := reverseGeocode(longitude, latitude)
-                if err != nil {
-                    log.Printf("Reverse geocoding failed for %s: %v", fileInfo.File, err)
-                    continue
-                }
-                writer.Write([]string{file.Name(), apiResponse})
-            }
-        }
-    }
+				rawLatitude := safeString(fileInfo.Fields, "GPSLatitude")
+				rawLongitude := safeString(fileInfo.Fields, "GPSLongitude")
+				latDirection := safeString(fileInfo.Fields, "GPSLatitudeRef")
+				lonDirection := safeString(fileInfo.Fields, "GPSLongitudeRef")
+				formattedLatitude, err := convertDMSToDecimal(rawLatitude, latDirection)
+				if err != nil {
+					log.Printf("Error converting latitude for %s: %v", fileInfo.File, err)
+					continue
+				}
+				formattedLongitude, err := convertDMSToDecimal(rawLongitude, lonDirection)
+				if err != nil {
+					log.Printf("Error converting longitude for %s: %v", fileInfo.File, err)
+					continue
+				}
+				apiResponse, err := reverseGeocode(formattedLongitude, formattedLatitude)
+				if err != nil {
+					log.Printf("Reverse geocoding failed for %s: %v", fileInfo.File, err)
+					continue
+				}
+				writer.Write([]string{file.Name(), rawLatitude, rawLongitude, fmt.Sprintf("%f", formattedLatitude), fmt.Sprintf("%f", formattedLongitude), apiResponse})
+			}
+		}
+	}
+}
+
+func isSupportedImageFile(fileName string) bool {
+	lowerFileName := strings.ToLower(fileName)
+	return strings.HasSuffix(lowerFileName, ".jpg") || strings.HasSuffix(lowerFileName, ".jpeg") || strings.HasSuffix(lowerFileName, ".tif") || strings.HasSuffix(lowerFileName, ".tiff")
 }
 
 func safeString(fields map[string]interface{}, key string) string {
-    if val, ok := fields[key]; ok && val != nil {
-        return fmt.Sprint(val)
-    }
-    return ""
+	if val, ok := fields[key]; ok && val != nil {
+		return fmt.Sprint(val)
+	}
+	return ""
 }
-
