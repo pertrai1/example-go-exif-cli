b42f693cd9892db6559cad52d2d8baee6fae639f
Rob Simpson <rsimpson2@me.com>
2024-04-24 20:43:53 -0400

    add arcgic reversegeocode

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+    "bytes"
     "encoding/csv"
+    "encoding/json"
     "fmt"
     "io/ioutil"
     "log"
+    "net/http"
     "os"
     "path/filepath"
+    "strconv"
+    "strings"
 
     "github.com/barasher/go-exiftool"
 )
+
+func reverseGeocode(longitude, latitude float64) (string, error) {
+    baseURL := "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode"
+    params := fmt.Sprintf("?location=%f,%f&outSR=4326&f=json", longitude, latitude)
+    response, err := http.Get(baseURL + params)
+    if err != nil {
+        return "", err
+    }
+    defer response.Body.Close()
+
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        return "", err
+    }
+
+    var prettyJSON bytes.Buffer
+    if err := json.Indent(&prettyJSON, body, "", "\t"); err != nil {
+        return "", err
+    }
+
+    return prettyJSON.String(), nil
+}
 
 func main() {
     if len(os.Args) < 2 {
@@ -37,28 +64,27 @@ func main() {
 
     writer := csv.NewWriter(csvFile)
     defer writer.Flush()
-
-    // Write CSV header
-    writer.Write([]string{"Filename", "Latitude", "LatitudeRef", "Longitude", "LongitudeRef", "Altitude", "AltitudeRef"})
+    writer.Write([]string{"Filename", "Reverse Geocode Response"})
 
     for _, file := range files {
         filePath := filepath.Join(dirPath, file.Name())
-        if !file.IsDir() && (filepath.Ext(file.Name()) == ".jpg" || filepath.Ext(file.Name()) == ".jpeg" || filepath.Ext(file.Name()) == ".tif" || filepath.Ext(file.Name()) == ".tiff") {
-            fileInfos := et.ExtractMetadata(filePath)
-            for _, fileInfo := range fileInfos {
+        fileExt := strings.ToLower(filepath.Ext(file.Name()))
+        if !file.IsDir() && (fileExt == ".jpg" || fileExt == ".jpeg" || fileExt == ".tif" || fileExt == ".tiff") {
+            fileMetadatas := et.ExtractMetadata(filePath)
+            for _, fileInfo := range fileMetadatas {
                 if fileInfo.Err != nil {
                     log.Printf("Error concerning %s: %v", fileInfo.File, fileInfo.Err)
                     continue
                 }
 
-                latitude := safeString(fileInfo.Fields, "GPSLatitude")
-                latitudeRef := safeString(fileInfo.Fields, "GPSLatitudeRef")
-                longitude := safeString(fileInfo.Fields, "GPSLongitude")
-                longitudeRef := safeString(fileInfo.Fields, "GPSLongitudeRef")
-                altitude := safeString(fileInfo.Fields, "GPSAltitude")
-                altitudeRef := safeString(fileInfo.Fields, "GPSAltitudeRef")
-
-                writer.Write([]string{file.Name(), latitude, latitudeRef, longitude, longitudeRef, altitude, altitudeRef})
+                latitude, _ := strconv.ParseFloat(safeString(fileInfo.Fields, "GPSLatitude"), 64)
+                longitude, _ := strconv.ParseFloat(safeString(fileInfo.Fields, "GPSLongitude"), 64)
+                apiResponse, err := reverseGeocode(longitude, latitude)
+                if err != nil {
+                    log.Printf("Reverse geocoding failed for %s: %v", fileInfo.File, err)
+                    continue
+                }
+                writer.Write([]string{file.Name(), apiResponse})
             }
         }
     }
